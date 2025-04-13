c162b3eae8b761dcea2b3022b394541116c05b98
Gustavo Silva <gustavomachado.gms@hotmail.com>
2025-04-13 20:17:44 -0300

    quicksort modified

--- QuickSort/main.c
+++ QuickSort/main.c
@@ -3,9 +3,11 @@
 #include<time.h>
 #include"quickSort.h"
 
-#define MAX 100000
+#define MAX 20
 #define MAX_VALUE 20000
 
+int* allocateArray(int size);
+int* freeArray(int* array);
 void generateRandomArray(int values[], int size, int maxValue);
 void printArray(int values[], int size);
 
@@ -13,18 +15,40 @@ int main(void) {
 
     srand(time(NULL));
 
-    int values[MAX];
+    int* values;
     int recursionsCounter;
     int swapCounter;
+
+    values = allocateArray(MAX);
 
     generateRandomArray(values, MAX, MAX_VALUE);
 
     quickSort(values, 0, MAX-1, MEDIAN_OF_THREE, HOARE, &recursionsCounter, &swapCounter);
 
+    printArray(values, MAX);
+
+    values = freeArray(values);
+
     // printArray(values, MAX);
+
     printf("---- OK ----\n");
 
     return 0;
+}
+
+int* allocateArray(int size) {
+
+    int* array = (int*) malloc(size * sizeof(int));
+
+    return array;
+
+}
+
+int* freeArray(int* array) {
+    
+    free(array);
+    return NULL;
+
 }
 
 void generateRandomArray(int values[], int size, int maxValue) {
